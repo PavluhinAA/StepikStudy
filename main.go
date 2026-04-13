b001ea69f03e9959f762f45aa21f1cc085cd364d
Anton <pavlukhin.ant0n@yandex.ru>
2026-04-13 18:44:01 +0300

    N5

--- main.go
+++ main.go
@@ -1,20 +1,26 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strconv"
+	"strings"
+)
 
 func main() {
-	workArray := [10]uint8{}
-	for i := 0; i < len(workArray); i++ {
-		fmt.Scanf("%d", &workArray[i])
+	var a int
+	fmt.Scan(&a)
+	fmt.Println(recursTest(a))
+}
+func recursTest(a int) int { //Чистый тест, никакого практического применения не имеет
+	if a < 10 {
+		return a
 	}
-
-	for i := 0; i < 3; i++ {
-		var ind1, ind2 int
-		fmt.Scan(&ind1, &ind2)
-		workArray[ind1], workArray[ind2] = workArray[ind2], workArray[ind1]
-
+	str := strconv.Itoa(a)
+	arr := strings.Split(str, "")
+	a = 0
+	for i := 0; i < len(arr); i++ {
+		b, _ := strconv.Atoi(arr[i])
+		a += b
 	}
-	for i := 0; i < len(workArray); i++ {
-		fmt.Print(workArray[i], " ")
-	}
+	return recursTest(a)
 }
